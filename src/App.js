7e3dca8cf1e108c2dc1be7257e20af89f8c25cc3
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2023-11-12 04:08:46 +0000

    sign in

--- src/App.js
+++ src/App.js
@@ -5,61 +5,23 @@ import ImageLinkForm from './ImageLinkForm/ImageLinkForm';
 import Rank from './Rank/Rank';
 import ParticlesEffect from './Particles/Particles';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import './App.css';
-
-
-// //CLARIFAI
-// // Your PAT (Personal Access Token) can be found in the portal under Authentification
-// const PAT = '5d64e4b1500c49f68d6173563b6facb9';
-// // Specify the correct user_id/app_id pairings
-// // Since you're making inferences outside your app's scope
-// const USER_ID = 'clarifai';       
-// const APP_ID = 'main';
-// // Change these to whatever model and image URL you want to use
-// const MODEL_ID = 'face-detection';
-// const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
-// const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
-// ///////////////////////////////////////////////////////////////////////////////////
-// // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-// ///////////////////////////////////////////////////////////////////////////////////
-// const raw = JSON.stringify({
-//     "user_app_id": {
-//         "user_id": USER_ID,
-//         "app_id": APP_ID
-//     },
-//     "inputs": [
-//         {
-//             "data": {
-//                 "image": {
-//                     "url": IMAGE_URL
-//                 }
-//             }
-//         }
-//     ]
-// });
-// const requestOptions = {
-//     method: 'POST',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Authorization': 'Key ' + PAT
-//     },
-//     body: raw
-// };
-// // END OF CLARIFAI
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      id:'',
       input:'',
       imageUrl:'',
       box:{},
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
   calculcateFaceLocation = (data) => {
-    // console.log("calculcateFaceLocation: ",data);
-    // console.log("Short: ",data.outputs[0].data.regions[0].region_info.bounding_box);
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
@@ -74,13 +36,12 @@ class App extends Component {
 
   displayFaceBox = (box) => {
     this.setState({box: box});
-    console.log(box);
   }
 
   onInputChange = (event)=>{
     this.setState({input: event.target.value});    
   }
-
+  
   onButtonSubmit = ()=>{
     this.setState({imageUrl: this.state.input});
     // Your PAT (Personal Access Token) can be found in the portal under Authentification
@@ -90,8 +51,13 @@ class App extends Component {
     const USER_ID = 'clarifai';       
     const APP_ID = 'main';
     // Change these to whatever model and image URL you want to use
+    //Models - https://clarifai.com/explore/models
+    //age-demographics-recognition -- https://clarifai.com/clarifai/main/models/age-demographics-recognition
+    //general-image-recognition -- https://clarifai.com/clarifai/main/models/general-image-recognition
+    //color-recognition -- https://clarifai.com/clarifai/main/models/color-recognition
+    //general-image-detection -- https://clarifai.com/clarifai/main/models/general-image-detection
+    //face-detection -- https://clarifai.com/clarifai/main/models/face-detection
     const MODEL_ID = 'face-detection';
-    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';     
     const IMAGE_URL = this.state.input;
     const raw = JSON.stringify({
         "user_app_id": {
@@ -117,27 +83,46 @@ class App extends Component {
         body: raw
     };
     // END OF CLARIFAI
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
     .then(response => response.json())
     .then(result => this.displayFaceBox(this.calculcateFaceLocation(result))) 
     .catch(error => console.log('error', error));
     
   }
-  //console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+
+  onRouteChange = (route) => {
+    if (route === 'signout'){
+      this.setState({isSignedIn: false})
+    } else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+    
   render(){
-  return (
+    const {isSignedIn, imageUrl, route, box} = this.state;
+  return [
     <div className='App'>
-      <Navigation/>
-      <ParticlesEffect/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm 
-        onInputChange={this.onInputChange}
-        onButtonSubmit={this.onButtonSubmit}
-      />
-      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>    
+      <ParticlesEffect/>  
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+      {route === 'home'        
+          ?<div>
+            <Logo/>
+            <Rank/>
+            <ImageLinkForm 
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl}/>
+          </div>
+          :(
+            route === 'signin'
+            ?<Signin onRouteChange={this.onRouteChange}/>
+            :<Register onRouteChange={this.onRouteChange}/>
+          )
+      }
     </div>
-  );
+  ];
   }
 }
 
