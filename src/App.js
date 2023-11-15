a821651e4b1ed92f0cc0ee2dbcb464810d8d7116
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2023-11-15 21:00:23 +0000

    security updates

--- src/App.js
+++ src/App.js
@@ -9,6 +9,21 @@ import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import './App.css';
 
+const initialState={
+  input:'',
+  imageUrl:'',
+  box:{},
+  route: 'signin',
+  isSignedIn: false,
+  user:{
+    id:'',
+    name:'',
+    email:'',
+    entries: 0,
+    joined: ''}
+}
+
+
 class App extends Component {
   constructor() {
     super();
@@ -117,6 +132,7 @@ class App extends Component {
         .then(count =>{
           this.setState(Object.assign(this.state.user, {entries: count}))
         })
+        .catch(console.log);
       }
       this.displayFaceBox(this.calculcateFaceLocation(result))
     }) 
@@ -126,7 +142,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout'){
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home'){
       this.setState({isSignedIn: true})
     }
