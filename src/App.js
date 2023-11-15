056f13f49848750898811c6359177f3f373cf890
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2023-11-15 23:07:43 +0000

    update

--- src/App.js
+++ src/App.js
@@ -78,46 +78,13 @@ class App extends Component {
   
   onButtonSubmit = ()=>{
     this.setState({imageUrl: this.state.input});
-    // Your PAT (Personal Access Token) can be found in the portal under Authentification
-    const PAT = '5d64e4b1500c49f68d6173563b6facb9';
-    // Specify the correct user_id/app_id pairings
-    // Since you're making inferences outside your app's scope
-    const USER_ID = 'clarifai';       
-    const APP_ID = 'main';
-    // Change these to whatever model and image URL you want to use
-    //Models - https://clarifai.com/explore/models
-    //age-demographics-recognition -- https://clarifai.com/clarifai/main/models/age-demographics-recognition
-    //general-image-recognition -- https://clarifai.com/clarifai/main/models/general-image-recognition
-    //color-recognition -- https://clarifai.com/clarifai/main/models/color-recognition
-    //general-image-detection -- https://clarifai.com/clarifai/main/models/general-image-detection
-    //face-detection -- https://clarifai.com/clarifai/main/models/face-detection
-    const MODEL_ID = 'face-detection';
-    const IMAGE_URL = this.state.input;
-    const raw = JSON.stringify({
-        "user_app_id": {
-            "user_id": USER_ID,
-            "app_id": APP_ID
-        },
-        "inputs": [
-            {
-                "data": {
-                    "image": {
-                        "url": IMAGE_URL
-                    }
-                }
-            }
-        ]
-    });
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': 'Key ' + PAT
-        },
-        body: raw
-    };
-    // END OF CLARIFAI
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+    fetch('http://localhost:3001/imageurl',{
+          method: 'post',
+          headers:{'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            input: this.state.input
+          })
+        })
     .then(response => response.json())
     .then(result => {
       if (result){
